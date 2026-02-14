9d7b263d2d4e34c9b1558ad1287490a275db57f6
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2026-02-14 20:11:27 +0530

    [dasun] fix build issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.motivation_lock"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.example.motivation_lock"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
